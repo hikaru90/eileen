18686f59f474710574a3343c1ec62219d3597212
Superalex <alexbueckner@gmail.com>
2023-03-27 00:55:11 +0200

    recommit

--- plugins/piniaStore.ts
+++ plugins/piniaStore.ts
@@ -3,5 +3,5 @@ import { markRaw } from 'vue'
 export default defineNuxtPlugin(({ $pinia, $router }) => {
   if (process.client) {
     $pinia.use(({ store }) => { store.router = markRaw($router) })
-  }
+  } 
 })
