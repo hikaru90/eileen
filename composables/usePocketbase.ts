e4b03a0cbc0de1e8363f4203582be7ac3c7648a1
hikaru90 <alexbueckner@gmail.com>
2023-06-14 23:18:57 +0200

    change server url

--- composables/usePocketbase.ts
+++ composables/usePocketbase.ts
@@ -2,7 +2,7 @@ import { ref } from 'vue'
 import PocketBase from 'pocketbase'
 
 export default function usePocketBase() {
-  const pb = new PocketBase('http://164.90.225.18');
+  const pb = new PocketBase('https://backend.dimplegoertz.de:8443');
 
   return {
     pb
