2e98268fba0c471859a6534408afd81e9ff94b6d
hikaru90 <alexbueckner@gmail.com>
2023-10-05 00:05:59 +0200

    add scroll to top functionality

--- components/UpButton.vue
+++ components/UpButton.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
-
   const state = reactive({
     threshold: 500,
     visible: false,
-  })
+  });
 
   const handleScroll = (event) => {
-    const scrollPosition = document.querySelector("#content-container").scrollTop
-    if(scrollPosition > state.threshold) state.visible = true
-    else state.visible = false
+    const scrollPosition = document.querySelector("#content-container").scrollTop;
+    if (scrollPosition > state.threshold) state.visible = true;
+    else state.visible = false;
+  };
+
+  const scrollToTop = () => {
+    if (process.client) {
+      document.querySelector("#content-container").scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
   };
 
   onMounted(() => {
@@ -25,11 +33,13 @@
 </script>
 
 <template>
-  <div :class="[ state.visible ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none' ]"
+  <button
+    @click="scrollToTop"
+    :class="[state.visible ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none']"
     class="fixed bottom-6 right-10 flex items-center justify-center rounded-full shadow-xl bg-white p-4 border border-grey border-opacity-10 transition duration-500"
   >
     <!-- <div class="flex-shrink-0 w-5 h-5 rounded-full bg-gradient-to-b from-gold via-lightGold to-darkGold"> -->
     <nuxt-icon name="icon-arrow_up" class="text-xl text-darkGrey" />
     <!-- </div> -->
-  </div>
+  </button>
 </template>
