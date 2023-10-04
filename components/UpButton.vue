f0c0bb67f77b83b25e1388de71f0ca5295fa3ca0
hikaru90 <alexbueckner@gmail.com>
2023-10-05 00:08:46 +0200

    change up button design

--- components/UpButton.vue
+++ components/UpButton.vue
@@ -36,7 +36,7 @@
   <button
     @click="scrollToTop"
     :class="[state.visible ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none']"
-    class="fixed bottom-6 right-10 flex items-center justify-center rounded-full shadow-xl bg-white p-4 border border-grey border-opacity-10 transition duration-500"
+    class="fixed bottom-4 md:bottom-6 right-5 md:right-10 flex items-center justify-center rounded-full shadow-xl bg-white p-2 md:p-4 border border-grey border-opacity-10 transition duration-500"
   >
     <!-- <div class="flex-shrink-0 w-5 h-5 rounded-full bg-gradient-to-b from-gold via-lightGold to-darkGold"> -->
     <nuxt-icon name="icon-arrow_up" class="text-xl text-darkGrey" />
